7fba2cfa3ca7e3b54871caedc32426a234e56afe
Rafael-Icaro-Sala <rafael_tironi1@estudante.sesisenai.org.br>
2026-09-17 11:19:30 -0300

    add: arrumei o cadastro ( parte 1)

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,7 @@ CREATE DATABASE banco_exemplo;
 
 USE banco_exemplo;
 
-CREATE TABLE cliente (
+CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
